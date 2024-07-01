56d5a3d4fbca130df5efe515885fd3edaf55050f
Santiago Ariel <santiagoarielv98@gmail.com>
2024-07-01 17:39:13 -0300

    cambios 2

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
-{
-  base: "/crui-prueba_mapa/";
-}
+/**
+ * @type {import('vite').UserConfig}
+ */
+export default {
+  base: "/crui-prueba_mapa/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        mapa: "./mapa.html",
+      },
+    },
+  },
+};
